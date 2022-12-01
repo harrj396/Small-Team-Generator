1702576eded90c6cfd46914837941c32a8995009
harrj396 <ahletmethink@gmail.com>
2022-12-01 03:41:59 -0500

    attempted troubleshooting type string error

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const distPath = path.join(DIST_DIR, 'team.html');
 
 const render = require('./src/work-template.js');
 
-const teamMembers = [];
+const teamMembers = []
 
+function buildmanager() {
 inquirer
   .prompt([ {
     type:"input",
@@ -52,6 +53,7 @@ inquirer
   .catch((error) => {
     console.log(error)
   });
+}
 
 function buildengineer(){
   inquirer
@@ -145,26 +147,9 @@ function close(){
   fs.writeFile(distPath, render(teamMembers), (err) =>
     err ? console.error(err) : console.log("created HTML page!"))
 }
-// then write data to HTML
 
+buildmanager();
+// TODO: troubleshoot render(teamMembers) not loading properly
 
-
-  // start inquierer prompt, starts with asking for managers name, then engineer name, then intern name.  Then generate the HTML page with that info.  
-// push each new object into an array, then add array to HTML.
-
-// function for creating manager - inquirer questions
-  // take those questions and create a new Manager with the user provided answers
-  // push that new Manager to the team members array
-
-  // follow the same pattern for each type of employee
-  // build a function for them that uses inquirer
-
-
-// STRUCTURING IT
-
-// start with manager function, since every team needs a manager
-// at the end of manager function, call a createTeam function
-
-// this function would simply ask the user which type of employee they would like to add, based on their choice, run the correesponding function
 
 // at the end, use fs to write file while sending the team array over to the function you brought in from page-template.js
